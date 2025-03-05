5536374d3bac6b1786c152c9c7a66ec0a3bc3ab8
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-05 12:13:40 +0100

    add some mor content.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@
 import Header from "./Header.tsx";
 import Blog from "./Blog.tsx";
 import Footer from "./Footer.tsx";
+import Profile from "./Profile.tsx";
 
 export default function App() {
     return (
@@ -11,6 +12,9 @@ export default function App() {
             Hello World!<br/>&nbsp;<br/>
             Here are the blog entries:<br/>
             <Blog/>
+            <br/>&nbsp;<br/>
+            <Profile/>
+            <br/>&nbsp;<br/>
             And here comes the footer:<br/>
             <Footer/>
         </div>
---
+++ src/Profile.tsx
@@ -0,0 +1,5 @@
+export default function Profile() {
+    return (
+        <div>I'm Gregor, a coming soon Java fullstack developer!</div>
+    )
+}
